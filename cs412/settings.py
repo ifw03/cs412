c1f63594ea9aead5e0660e3e1f8e82b29e71000d
Ignacio <ifw@bu.edu>
2026-06-02 12:13:21 -0400

    fix static settings to match deployment spec

--- cs412/settings.py
+++ cs412/settings.py
@@ -121,8 +121,12 @@ USE_TZ = True
 
 import os
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = "media/"
