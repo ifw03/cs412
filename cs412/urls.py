c85a569c265a74477051115849d37e3bc400e389
Ignacio <ifw@bu.edu>
2026-06-02 11:38:43 -0400

    add mini_insta post/photo migrations

--- cs412/urls.py
+++ cs412/urls.py
@@ -26,6 +26,5 @@ urlpatterns =[
     path('quotes/', include('quotes.urls')),
     path('restaurant/', include('restaurant.urls')),
     path('mini_insta/', include('mini_insta.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
